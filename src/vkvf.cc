0af70b6f18afd29682244ef2872ffc131c0184a7
yahor.subach <yahor.subach@soft-fx.lv>
2019-07-18 19:43:17 +0300

    logical device creation

--- src/vkvf.cc
+++ src/vkvf.cc
@@ -66,6 +66,8 @@ namespace vkvf
 			{
 				InitPhysicalDeviceProperties(vk_physical_devices_[i]);
 				InitPhysicalDeviceQueueFamaliesProperties(vk_physical_devices_[i]);
+				if (!InitLogicalDevice(vk_physical_devices_[i]))
+					return false;
 			}
 
 			return true;
@@ -75,9 +77,9 @@ namespace vkvf
 		{
 			using PhDevPropMapType = std::map<VkPhysicalDevice, VkPhysicalDeviceMemoryProperties>;
 
-			auto it = vk_physical_devices_propeties_.find(physical_device);
+			auto it = vk_physical_devices_propeties_.lower_bound(physical_device);
 
-			if (it == vk_physical_devices_propeties_.end())
+			if (it == vk_physical_devices_propeties_.end() || it->first != physical_device)
 			{
 				it = vk_physical_devices_propeties_.emplace_hint(it,
 					std::piecewise_construct, std::forward_as_tuple(physical_device), std::forward_as_tuple());
@@ -90,9 +92,9 @@ namespace vkvf
 		{
 			using PhDevFamQMapType = std::map<VkPhysicalDevice, std::vector<VkQueueFamilyProperties>>;
 
-			auto it = vk_physical_devices_queue_famalies_propeties_.find(physical_device);
+			auto it = vk_physical_devices_queue_famalies_propeties_.lower_bound(physical_device);
 
-			if (it == vk_physical_devices_queue_famalies_propeties_.end())
+			if (it == vk_physical_devices_queue_famalies_propeties_.end() || it->first != physical_device)
 			{
 				uint32_t device_queue_famaly_prop_cnt;
 				vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &device_queue_famaly_prop_cnt, nullptr);
@@ -106,6 +108,55 @@ namespace vkvf
 			}
 		}
 
+
+		void InitPhysicalDeviceFeatures(VkPhysicalDevice physical_device)
+		{
+
+			using PhDevPropMapType = std::map<VkPhysicalDevice, VkPhysicalDeviceFeatures>;
+
+			auto it = vk_physical_devices_features_.lower_bound(physical_device);
+
+			if (it == vk_physical_devices_features_.end() || it->first != physical_device)
+			{
+				it = vk_physical_devices_features_.emplace_hint(it,
+					std::piecewise_construct, std::forward_as_tuple(physical_device), std::forward_as_tuple());
+				vkGetPhysicalDeviceFeatures(physical_device, &it->second);
+
+			}
+		}
+
+		bool InitLogicalDevice(VkPhysicalDevice physical_device)
+		{
+			VkDeviceCreateInfo logical_device_create_info;
+			logical_device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+			logical_device_create_info.pNext = nullptr;
+			logical_device_create_info.flags = 0;
+			logical_device_create_info.enabledExtensionCount = 0;
+			logical_device_create_info.enabledLayerCount = 0;
+			logical_device_create_info.ppEnabledExtensionNames = nullptr;
+			logical_device_create_info.ppEnabledLayerNames = nullptr;
+			logical_device_create_info.pEnabledFeatures = &vk_physical_devices_features_[physical_device];
+
+
+
+			VkDeviceQueueCreateInfo queueu_create_info;
+			queueu_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueu_create_info.flags = 0;
+			queueu_create_info.queueFamilyIndex = 0;
+			queueu_create_info.queueCount = vk_physical_devices_queue_famalies_propeties_[physical_device][0].queueCount;
+			queueu_create_info.pQueuePriorities = nullptr;
+			queueu_create_info.pNext = nullptr;
+
+			logical_device_create_info.queueCreateInfoCount = 1;
+			logical_device_create_info.pQueueCreateInfos = &queueu_create_info;
+
+			vk_logical_devices_.emplace_back();
+
+			if (vkCreateDevice(physical_device, &logical_device_create_info, nullptr, &vk_logical_devices_[vk_logical_devices_.size() - 1]) != VK_SUCCESS)
+				return false;
+			return true;
+		}
+
 		bool vk_init_success_;
 		std::unique_ptr<VkApplicationInfo> application_info_;
 		std::unique_ptr<VkInstance> vk_instance_;
@@ -113,6 +164,9 @@ namespace vkvf
 		std::vector<VkPhysicalDevice> vk_physical_devices_;
 		std::map<VkPhysicalDevice, VkPhysicalDeviceMemoryProperties> vk_physical_devices_propeties_;
 		std::map<VkPhysicalDevice, std::vector<VkQueueFamilyProperties>> vk_physical_devices_queue_famalies_propeties_;
+		std::map<VkPhysicalDevice, VkPhysicalDeviceFeatures> vk_physical_devices_features_;
+
+		std::vector<VkDevice> vk_logical_devices_;
 
 	};
 
