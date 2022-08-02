93aa7b5a9981f271123bf6ec309c0ae7583fbc47
egorqwq <egorqwq@yandex.ru>
2022-08-03 01:09:33 +0300

    pbr

--- shaders/collect_g_buffers.frag
+++ shaders/collect_g_buffers.frag
@@ -1,14 +1,23 @@
 #version 450
 
+#include "ggx.glsl"
+
 layout(set = 0, binding = 0) uniform sampler2D GBuffers_Albedo;
 layout(set = 0, binding = 1) uniform sampler2D GBuffers_Position;
 layout(set = 0, binding = 2) uniform sampler2D GBuffers_Normal;
 
+layout(set = 1, binding = 0) uniform CameraPositionAndViewProjMat_0 {
+    vec4 position;
+    mat4 projViewMatrix;
+} camera;
 
 
 layout(location = 0) in vec3 fragPosition;
 
 layout(location = 0) out vec4 outColor;
+
+
+
 
 void main() {
 
@@ -19,6 +28,32 @@ void main() {
 
 	if(fragPosition.y > 0)
 		texColor = texture(GBuffers_Normal, (fragPosition.xy));
+
+	if(fragPosition.x > 0 && fragPosition.y > 0)
+	{
+		
+		vec3 light_pos = vec3(1,3,4);
+		
+		vec3 unit_normal = normalize(texture(GBuffers_Normal, (fragPosition.xy)).xyz); 
+		
+		vec3 position = texture(GBuffers_Position, (fragPosition.xy)).xyz;
+		vec3 albedo = texture(GBuffers_Albedo, (fragPosition.xy)).xyz;
+		
+		vec3 unit_view_direction = normalize(camera.position.xyz - position);
+		vec3 unit_light_direction = normalize(light_pos - position);
+
+		float roughness = 0.5;
+		vec3 R0 = vec3(0.1,0.1,0.1);
+
+		
+
+		vec3 Reflected = F(unit_light_direction, unit_normal, R0);
+		vec3 Spek = CookTorrance_GGX(unit_view_direction, unit_light_direction, unit_normal, roughness, R0);
+		vec3 Diffused = 1 - Reflected;
+
+		texColor = vec4(albedo * Diffused * clamp(dot(unit_normal, unit_light_direction),0,1) / M_PI + Spek, 1); 
+
+	}
 
 	outColor =  texColor;
 }
---
+++ shaders/ggx.glsl
@@ -0,0 +1,52 @@
+
+#define M_PI 3.1415926535897932384626433832795
+
+float Hi(vec3 direction, vec3 normal)
+{
+	return dot(direction, normal) > 0 ? 1 : 0;
+}
+
+float G_Partial(vec3 unit_direction, vec3 unit_normal, float roughness)
+{
+	float cos_theta = dot(unit_direction, unit_normal);
+	float tan2 = 1 - 1 / (cos_theta * cos_theta);
+	float roughness2 = roughness * roughness;
+
+	return Hi(unit_direction, unit_normal) * (2 / (1 + sqrt(1 + roughness2*roughness2*tan2)));
+}
+
+float G(vec3 unit_view_direction, vec3 unit_light_direction,  vec3 unit_normal, float roughness)
+{
+	return G_Partial(unit_view_direction, unit_normal, roughness) * G_Partial(unit_light_direction, unit_normal, roughness);
+}
+
+float D(vec3 unit_view_direction, vec3 unit_light_direction,  vec3 unit_normal, float roughness)
+{
+	float roughness2 = roughness * roughness;
+	float roughness4 = roughness2 * roughness2;
+
+	vec3 H = normalize((unit_view_direction + unit_light_direction) / 2);
+
+	float cos_theta_m = dot(unit_normal, H);
+	float cos2 = cos_theta_m*cos_theta_m;
+	float cos4 = cos2*cos2;
+	float tan2 = 1 - 1 / (cos_theta_m * cos_theta_m);
+
+	return roughness4 / (M_PI * cos4 * (roughness4 + tan2) * (roughness4 + tan2));
+}
+
+// FresnelSchlick
+vec3 F(vec3 unit_light_direction,  vec3 unit_normal, vec3 R0)
+{
+	float cos_theta = clamp(dot(unit_light_direction, unit_normal), 0, 1);
+	return R0 + (1 - R0)*pow(1 - cos_theta, 5);
+}
+
+vec3 CookTorrance_GGX(vec3 unit_view_direction, vec3 unit_light_direction,  vec3 unit_normal, float roughness, vec3 Fresnel_R0)
+{
+	float D = D(unit_view_direction, unit_light_direction, unit_normal, roughness);
+	vec3 F = F(unit_light_direction, unit_normal, Fresnel_R0);
+	float G = G(unit_view_direction, unit_light_direction, unit_normal, roughness);
+
+	return D * F * G / (4 * dot(unit_view_direction, unit_normal) * dot(unit_normal, unit_light_direction));
+}
